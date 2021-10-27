0d1f7fd71da156e89ac2d55ed8076fda67ab2d14
Gokul-1995 <92993326+Gokul-1995@users.noreply.github.com>
2021-10-27 08:31:47 +0530

    Add files via upload

---
+++ day4-task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] numbers;
+            Console.WriteLine("ENTER THE SIZE OF THE ARRAY PLEASE!");
+            n = Convert.ToInt32(Console.ReadLine());
+            numbers = new int[n];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            double highestSalary = numbers.Max();
+
+            double lowestSalary = numbers.Min();
+
+            double averageSalary = numbers.Sum() / numbers.Length;
+            Console.WriteLine("highestSalary is:{0}\nlowestSalary is:{1}\naverageSalary is:{2}", highestSalary, lowestSalary, averageSalary);
+
+         
+        }
+    }
+}
