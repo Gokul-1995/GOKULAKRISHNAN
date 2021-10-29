7f25a31a74e3058784659f0d8b767651a112c08b
Gokul-1995 <92993326+Gokul-1995@users.noreply.github.com>
2021-10-29 09:47:26 +0530

    Add files via upload

---
+++ DAY6_TASK1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY6_TASK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your Name,Age,Address,Contact Number,Qalification ,Experience :");
+            String Details = Console.ReadLine();
+
+            String[] detail = Details.Split(',');
+            foreach (String all in detail)
+
+            {
+                Console.WriteLine(all);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
